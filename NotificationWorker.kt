4b0a8a7f0753f782acd2da8fe9b3c4cd8df13fb5
likhithakavitha <160044277+likhithakavitha@users.noreply.github.com>
2026-05-17 14:33:51 +0530

    Add NotificationWorker for vaccination notifications
    Implement NotificationWorker to handle vaccination alerts.

--- NotificationWorker.kt
+++ NotificationWorker.kt
@@ -1,1 +1,42 @@
+package com.example.gramavaxi
 
+import android.app.*
+import android.content.Context
+import androidx.core.app.NotificationCompat
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+
+class NotificationWorker(
+    context: Context,
+    params: WorkerParameters
+) : Worker(context, params) {
+
+    override fun doWork(): Result {
+
+        val manager =
+            applicationContext.getSystemService(
+                Context.NOTIFICATION_SERVICE
+            ) as NotificationManager
+
+        val channel = NotificationChannel(
+            "vaccine_channel",
+            "Vaccination Alerts",
+            NotificationManager.IMPORTANCE_HIGH
+        )
+
+        manager.createNotificationChannel(channel)
+
+        val notification = NotificationCompat.Builder(
+            applicationContext,
+            "vaccine_channel"
+        )
+            .setContentTitle("Vaccination Camp")
+            .setContentText("Doctor arriving tomorrow")
+            .setSmallIcon(android.R.drawable.ic_dialog_info)
+            .build()
+
+        manager.notify(1, notification)
+
+        return Result.success()
+    }
+}
