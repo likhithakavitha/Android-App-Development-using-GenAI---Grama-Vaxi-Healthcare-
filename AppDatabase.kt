69cf30b4c469ac6bdc0cc7c7d6574c181050b8af
likhithakavitha <160044277+likhithakavitha@users.noreply.github.com>
2026-05-17 14:30:30 +0530

    Create AppDatabase for Room database setup

--- AppDatabase.kt
+++ AppDatabase.kt
@@ -1,1 +1,10 @@
+package com.example.gramavaxi
 
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [Animal::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun animalDao(): AnimalDao
+}
